d6e16aa4317322380e254eb9e13534a113a69281
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-06-03 22:12:08 +0200

    add templates and layouts

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(expressLayouts);
 
 app.get('/', (req, res) => res.render('index'));
 
