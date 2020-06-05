023c552cfaa45ebf816eee489583e3fdb88f7f45
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-06-05 23:57:28 +0200

    add mongo config

--- config/index.js
+++ config/index.js
@@ -8,5 +8,10 @@ module.exports = {
     },
     session: {
         secret: process.env['SESSION_SECRET']
+    },
+    mongo: {
+        url: process.env['MONGO_URL'],
+        user: process.env['MONGO_USER'],
+        password: process.env['MONGO_PASSWORD']
     }
 };
