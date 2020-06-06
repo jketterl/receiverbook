9a0e6f7426a6b05bbef2267ea6ac98aae67fffc6
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-06-06 13:58:16 +0200

    debug

--- app/mongoose.js
+++ app/mongoose.js
@@ -6,6 +6,7 @@ module.exports.setup = async () => {
     const mongoUrl = new URL(config.mongo.url);
     mongoUrl.username = config.mongo.user;
     mongoUrl.password = config.mongo.password;
+    console.info("connecting to: " + mongoUrl.toString());
     await mongoose.connect(mongoUrl.toString(), { useNewUrlParser: true, useUnifiedTopology: true })
     return mongoose
 }
