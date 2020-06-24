43ef07e38870d7e4778402810891f398320a2d71
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-06-24 23:02:09 +0200

    number of stations

--- app/models/Station.js
+++ app/models/Station.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const stationSchema = new mongoose.Schema({
     label: String,
     owner: String,
-    receivers: {
+    receivers: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Receiver'
-    }
+    }]
 });
 
 module.exports = mongoose.model('Station', stationSchema);
