27ec46466463d344e81fed4143f9394802607503
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-12-16 21:50:22 +0100

    add protection against known zlib error

--- crawler.js
+++ crawler.js
@@ -1,5 +1,15 @@
 const CrawlerService = require('./app/service/CrawlerService');
 
+process.on('uncaughtException', (err) => {
+    if (err.message == 'unexpected end of file' && err.errno == -5 && err.code == 'Z_BUF_ERROR') {
+        console.info('ignoring zlib error');
+        return;
+    }
+    console.error(err);
+    console.error(err.stack);
+    process.exit(1);
+});
+
 require('./app/mongoose').setup().then( () => {
     console.info('crawler init complete, starting loop');
 
